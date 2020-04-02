e9ebec4ffd1ae81263bf10f31483982f44c3aeae
Martin Sehnoutka <msehnout@redhat.com>
2020-04-02 17:01:27 +0200

    switch branch

--- aarch64-git.sh
+++ aarch64-git.sh
@@ -6,7 +6,7 @@ pushd /tmp
 rm -rf osbuild-composer
 git clone https://github.com/msehnout/osbuild-composer.git
 pushd osbuild-composer
-git checkout aarch64-image-tests
+git checkout aarch64-boot-test
 dnf install go-rpm-macros make '@RPM Development Tools' -y
 dnf builddep osbuild-composer.spec -y
 make rpm
