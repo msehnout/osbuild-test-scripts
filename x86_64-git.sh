382b5d458f63648228c4f4dd67230d56a040d805
Martin Sehnoutka <msehnout@redhat.com>
2020-04-08 11:41:47 +0200

    switch x86_64 to upstream

--- x86_64-git.sh
+++ x86_64-git.sh
@@ -4,9 +4,8 @@ set -e
 
 pushd /tmp
 rm -rf osbuild-composer
-git clone https://github.com/msehnout/osbuild-composer.git
+git clone https://github.com/osbuild/osbuild-composer.git
 pushd osbuild-composer
-git checkout open-vm-tools-new-3
 dnf install go-rpm-macros make '@RPM Development Tools' -y
 dnf builddep golang-github-osbuild-composer.spec -y
 make rpm
