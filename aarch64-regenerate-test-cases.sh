858ee79cb9e2f19f6bb57d19110422b5e01b56f1
Martin Sehnoutka <msehnout@redhat.com>
2020-04-08 14:14:50 +0200

    script to generate test cases for arm

---
+++ aarch64-regenerate-test-cases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+pushd /tmp/osbuild-composer
+mkdir /tmp/store || touch /
+mkdir /tmp/output || touch /
+
+echo "fedora-30
+fedora-31
+fedora-32" |
+while read distro
+do
+  tools/test-case-generators/generate-test-cases --distro $distro --arch aarch64 --store /tmp/store/ --output /tmp/output/
+done
+
+popd
