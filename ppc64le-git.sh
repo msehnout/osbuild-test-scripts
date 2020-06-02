ef9f6c2019b595781b37d4ce23232f53ec1f15e0
Martin Sehnoutka <msehnout@redhat.com>
2020-06-02 14:16:35 +0200

    update

--- ppc64le-git.sh
+++ ppc64le-git.sh
@@ -6,7 +6,7 @@ pushd /tmp
 rm -rf osbuild-composer
 git clone https://github.com/msehnout/osbuild-composer.git
 pushd osbuild-composer
-git checkout ppc64le-support
+git checkout rhel8-ppc64le-support
 dnf install go-rpm-macros make '@RPM Development Tools' -y
 dnf builddep osbuild-composer.spec -y
 make rpm
