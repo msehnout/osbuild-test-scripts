42aa2ec5d53eb740b1b910641cc8a568e0ac877c
Martin Sehnoutka <msehnout@redhat.com>
2020-06-03 13:24:35 +0200

    update

--- ppc64le-git.sh
+++ ppc64le-git.sh
@@ -11,5 +11,9 @@ dnf install go-rpm-macros make '@RPM Development Tools' -y
 dnf builddep osbuild-composer.spec -y
 make rpm
 dnf install rpmbuild/RPMS/ppc64le/* -y
+rm -rf /osbuild
+mkdir -p /osbuild/store
+mkdir -p /osbuild/output
+tools/test-case-generators/generate-test-cases --distro rhel-8 --arch ppc64le --store /osbuild/store/ --output /osbuild/output/
 popd
 popd
