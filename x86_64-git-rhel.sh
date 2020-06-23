6a3876fb2290a033cf3c79f888a6428c648ee514
Martin Sehnoutka <msehnout@redhat.com>
2020-06-23 09:50:46 +0200

    update

---
+++ x86_64-git-rhel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+pushd /tmp
+rm -rf osbuild-composer
+git clone https://github.com/osbuild/osbuild-composer.git
+pushd osbuild-composer
+dnf install make '@RPM Development Tools' -y
+dnf builddep golang-github-osbuild-composer.spec -y
+make rpm
+dnf install rpmbuild/RPMS/x86_64/* -y
+popd
+popd
