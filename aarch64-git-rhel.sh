6ff723b65b7eec5015cb4cc675debbe415cf0d23
Martin Sehnoutka <msehnout@redhat.com>
2020-06-23 15:12:21 +0200

    update

--- aarch64-git-rhel.sh
+++ aarch64-git-rhel.sh
@@ -4,9 +4,8 @@ set -e
 
 pushd /tmp
 rm -rf osbuild-composer
-git clone https://github.com/msehnout/osbuild-composer.git
+git clone https://github.com/osbuild/osbuild-composer.git
 pushd osbuild-composer
-git checkout aarch64-boot-test
 dnf install make '@RPM Development Tools' -y
 dnf builddep osbuild-composer.spec -y
 make rpm
