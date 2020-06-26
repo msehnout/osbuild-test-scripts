6aabc050f6d0b717bcd1ebc797c95e7e27665d65
Martin Sehnoutka <msehnout@redhat.com>
2020-06-26 10:22:15 +0200

    update

--- x86_64-git.sh
+++ x86_64-git.sh
@@ -7,7 +7,7 @@ rm -rf osbuild-composer
 git clone https://github.com/osbuild/osbuild-composer.git
 pushd osbuild-composer
 dnf install go-rpm-macros make '@RPM Development Tools' -y
-dnf builddep golang-github-osbuild-composer.spec -y
+dnf builddep osbuild-composer.spec -y
 make rpm
 dnf install rpmbuild/RPMS/x86_64/* -y
 popd
