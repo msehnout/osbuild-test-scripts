42c5b34ab43094a4a562be691d3996c6a1929d9a
Martin Sehnoutka <msehnout@redhat.com>
2020-07-15 14:29:55 +0200

    update

--- aarch64-regenerate-test-cases.sh
+++ aarch64-regenerate-test-cases.sh
@@ -6,24 +6,21 @@ pushd /tmp
 rm -rf osbuild-composer
 git clone https://github.com/msehnout/osbuild-composer.git
 pushd osbuild-composer
-git checkout aarch64-boot-test
+git checkout fedora-selinux-build-pipeline
 dnf install go-rpm-macros make '@RPM Development Tools' -y
 dnf builddep osbuild-composer.spec -y
 dnf install osbuild -y
 popd
 popd
 
-cp ext4-format-request-map.json /tmp/osbuild-composer/tools/test-case-generators/format-request-map.json
-
 pushd /tmp/osbuild-composer
 mkdir -p /osbuild/store || touch /
 mkdir -p /osbuild/output || touch /
 
-echo "fedora-30
-fedora-31" |
+echo "fedora-32" |
 while read distro
 do
-  tools/test-case-generators/generate-test-cases --distro $distro --arch aarch64 --store /osbuild/store/ --output /osbuild/output/
+  tools/test-case-generators/generate-test-cases --distro $distro --arch aarch64 --store /osbuild/store/ --output /osbuild/output/ --image-types qcow2 --image-types ami --image-types openstack
 done
 
 popd
